cf8031d8b6938c579dcd6287f181bf970b21837c
naveenthenoob <naveendbg2016@gmail.com>
2021-12-27 18:58:22 +0530

    add

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+     
 class Stack:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        new_node = Node(data)
+         
+        if self.top == None:
+            self.top = new_node
+             
+        else:
+            new_node.next = self.top
+            self.top = new_node
+
+    def pop(self):
+         
+        if (self.top == None):
+            return None
+             
+        else:
+            current = self.top
+            self.top = self.top.next
+            return current.data
+
+    def peek(self):
+        if self.top == None:
+            return None
+        else:
+            return self.top.data
