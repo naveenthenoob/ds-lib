5354cf6c9b7a3fcb6e072d2b535e607ac3c5a93a
naveenthenoob <naveendbg2016@gmail.com>
2021-12-29 10:35:47 +0530

    update

--- stack/stack.py
+++ stack/stack.py
@@ -9,19 +9,15 @@ class Stack:
 
     def push(self, data):
         new_node = Node(data)
-         
         if self.top == None:
             self.top = new_node
-             
         else:
             new_node.next = self.top
             self.top = new_node
 
     def pop(self):
-         
         if (self.top == None):
-            return None
-             
+            return None 
         else:
             current = self.top
             self.top = self.top.next
