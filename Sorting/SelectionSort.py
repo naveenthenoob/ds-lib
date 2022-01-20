2ffb2966c46838a774dbf3139c972c0b9f0bfcd1
naveenthenoob <naveendbg2016@gmail.com>
2022-01-21 00:10:04 +0530

    SelectionSort added

---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,20 @@
+#find minimum value in which index in an array.
+def get_min_index(arr, start):
+    min_index = start
+
+    for i in range(start+1, len(arr)):
+        if(arr[i] < arr[min_index]):
+            min_index = i
+    return min_index
+
+
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n-1):
+        start = i
+        min_index = get_min_index(arr, start)
+        arr[min_index], arr[start] = arr[start], arr[min_index]
+    return arr
+arr=[1 ,7, 4, 86, 9,3, 2, 0, 6, 88, 44]
+print(selection_sort(arr))
