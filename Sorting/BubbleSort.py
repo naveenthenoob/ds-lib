0b465205c6b05ad9f14687cd3b44d2875004fc5b
naveenthenoob <naveendbg2016@gmail.com>
2022-01-20 10:51:19 +0530

    BubbleSort added

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,16 +1,18 @@
 def Bubblesort(arr):
     n = len(arr)
     for i in range(len(arr)):
+
         swapping_count = 0
         for j in range(n-i-1):
+
             if (arr[j] > arr[j+1]):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapping_count += 1
 
         if(swapping_count == 0):
+            # when array is sorted
             break
-arr = list(map(int,input().split( )))
+        
+arr =[2, 5, 7, 8, 55, 1, 9]
 Bubblesort(arr)
 print(arr)
-
-    
