caf0da3a685723095c8e393f8c4e987c769c3806
naveenthenoob <naveendbg2016@gmail.com>
2022-01-25 12:55:04 +0530

    insertion_sort updated

--- Sorting/insertionsort.py
+++ Sorting/insertionsort.py
@@ -1,8 +1,9 @@
 def insertion_sort(arr):
-    for i in range(1,len(arr)):
-        temp=arr[i]
-        j=i-1
-        while(j>=0 and arr[j] > temp):
-            arr[j+1] = arr[j]
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i- 1
+        while(j >= 0 and arr[j] > temp):
+
+            arr[j + 1] = arr[j]
             j-= 1
-        arr[j+1]=temp
+        arr[j+ 1] = temp
